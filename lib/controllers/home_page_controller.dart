4304e2cc340a34b3e17ac48cb3f88991da70a5e8
bidhan-skybase <bidhan@skybase.com.np>
2025-06-05 20:16:28 +0545

    feat: Add dynamic background colors

--- lib/controllers/home_page_controller.dart
+++ lib/controllers/home_page_controller.dart
@@ -1,32 +1,14 @@
+import 'package:demoapp/colors.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 //variable declaration
 //logic work
 // api calls
 class HomePageController extends GetxController {
+  Rx<Color> bgColor=AppColors.refreshingColor.obs;
 
-  //for making the variable observable.
-  RxInt count = 0.obs;
-
-  RxBool isLiked=true.obs;
-
-  void toggleLikeStatus(){
-    isLiked.value=!isLiked.value;
-  }
-
-  void increaseCount() {
-    count = count + 1;
-    print("the count is ${count}");
-  }
-
-
-  void decreaseCount() {
-    count = count - 1;
-    print("the count is ${count}");
-  }
-
-  void resetCount() {
-    count.value = 0;
-    print("the count is ${count}");
+  void onColorPressed(Color color){
+    bgColor.value=color;
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,57 +1,43 @@
+import 'package:demoapp/colors.dart';
 import 'package:demoapp/controllers/home_page_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
-  //to bring home page controller in the view
   final c = Get.put((HomePageController()));
 
   HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              //to reflect observable variable changes
-              Obx((){
-                return Text(
-                  c.count.value.toString(),
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w800),
-                );
-              }),
-             Text("this is the count"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(onPressed: (){c.decreaseCount();}, child: Text("Decrease")),
-                  ElevatedButton(onPressed: (){c.resetCount();}, child: Text("Reset")),
-                  ElevatedButton(onPressed: (){c.increaseCount();}, child: Text("Increase"))
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              InkWell(
-                onTap: (){
-                  c.toggleLikeStatus();
-                },
-                child: Obx((){
-                  if(c.isLiked.value==true){
-                   return Icon(Icons.favorite,color: Colors.red,);
-                  }else{
-                    return Icon(Icons.favorite_border,color: Colors.red,);
-                  }
-                }),
-              )
 
-            ],
+    return Obx((){
+      return Scaffold(
+        backgroundColor: c.bgColor.value,
+        appBar: AppBar(title: Text("Home Page")),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                ElevatedButton(onPressed: () {
+                  c.onColorPressed(AppColors.primaryColor);
+
+                }, child: Text("Primary color")),
+                ElevatedButton(
+                  onPressed: () {
+                    c.onColorPressed(AppColors.signUpColor);
+                  },
+                  child: Text("signUpColor color"),
+                ),
+                ElevatedButton(onPressed: () {
+                  c.onColorPressed(AppColors.refreshingColor);
+                }, child: Text("refreshing color")),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
+
   }
 }
